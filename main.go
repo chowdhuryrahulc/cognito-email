96013ddb9b16189f53fa5b71abbdcde3f7f5cd4e
Rahul Chowdhury <84246818+chowdhuryrahulc@users.noreply.github.com>
2023-01-14 17:07:29 +0530

    cmt

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+50,000 active users free
+in Image, user is front-end flutter app. Thats why microservices connections are important
+1) go to IAM, users, add users, add name, and enable programatic access, 
+attach policy: cognito power user, no need for key value so press next, Create User, 
+copy access key and secret key
+For Production, always create a custom IAM role
+secret_key: 
+*/
+
